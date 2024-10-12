90941e26f316fc907695acd2902967690232a348
Rodion Chernomordin <rodion.chernomordin@gmail.com>
2024-10-12 16:51:01 +0300

    fix bugs

--- src/transforms/wav_augs/__init__.py
+++ src/transforms/wav_augs/__init__.py
@@ -1,3 +1,3 @@
 from src.transforms.wav_augs.gain import Gain
 from src.transforms.wav_augs.filters import BandPassFilter, LowPassFilter, HighPassFilter
-from src.transforms.wav_augs.noise import ColoredNois
+from src.transforms.wav_augs.noise import ColoredNoise
